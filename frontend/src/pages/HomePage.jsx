adec780fd7a0c1192acec88368d913671d4b739f
ahamedkabir48 <ahamedkabir48@gmail.com>
2025-09-28 12:18:07 +0530

    Homepage.jsx commit

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import CreateEventForm from '../components/CreateEventForm';
-import EventList from '../components/EventList';
-import Header from '../components/Header';
+//import CreateEventForm from '../components/CreateEventForm';
+//import EventList from '../components/EventList';
+//import Header from '../components/Header';
 import './HomePage.css';
 
 
